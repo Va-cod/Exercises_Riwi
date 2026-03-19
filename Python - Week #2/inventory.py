1257d3e7b63fe57a5f42fe0e8e5d9727ffe57fb7
Va-cod <atenciodiazvalentina@gmail.com>
2026-03-18 21:22:40 -0500

    Task 2: function to add products

--- Python - Week #2/inventory.py
+++ Python - Week #2/inventory.py
@@ -11,3 +11,33 @@ def show_inventory():
     else:  
       for product in inventory:
         print(f"Name: {product['Name']}, Price: {product['Price']}, Quantity: {product['Quantity']}")
+
+# Function to add products
+def add_product():
+  name = input("\nName: ") # Ask for the product name
+  
+  # Data validation block
+  while True:
+    try:
+      price = float(input("Price: ")) # Ask for the product price
+      if price < 0: # Conditional to evaluate if the price is negative
+        print("Price must be positive.\n")
+        continue
+      break
+    except:
+      print("Error value\n")
+
+  # Data validation block
+  while True:
+    try:
+      quantity = int(input("Quantity: ")) # Ask for the product quantity
+      if quantity < 0: # Conditional to evaluate if the quantity is negative
+        print("Quantity must be positive.\n")
+        continue
+      break
+    except:
+      print("Error value\n")
+  
+  product = {"Name": name, "Price": price, "Quantity": quantity} # Dictionary with product data
+  inventory.append(product) # Dictionary added to list
+  print("Product added to inventory") 
